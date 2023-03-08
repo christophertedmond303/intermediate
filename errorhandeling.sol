2c1139500b819f9067be1fff80d64ea22f20d93b
christophertedmond303 <126945670+christophertedmond303@users.noreply.github.com>
2023-03-08 20:28:34 +0530

    Create errorhandeling.sol

---
+++ errorhandeling.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.13;
+
+contract Error {
+    function testRequire(uint _i) public pure {
+        
+        require(_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint _i) public pure {
+        
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint public num;
+
+    function testAssert() public view {
+       
+        assert(num == 0);
+    }
+
+    
+    error InsufficientBalance(uint balance, uint withdrawAmount);
+
+    function testCustomError(uint _withdrawAmount) public view {
+        uint bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({balance: bal, withdrawAmount: _withdrawAmount});
+        }
+    }
+}
